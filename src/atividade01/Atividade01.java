a190acfc23d5b12411881565f6795bd9bb70a4a2
bgr240403 <brunogr240403@gmail.com>
2023-11-23 21:08:17 -0300

    atividade01

---
+++ src/atividade01/Atividade01.java
@@ -0,0 +1,18 @@
+
+package atividade01;
+
+import view.TelaPrincipal;
+
+
+public class Atividade01 {
+
+
+    public static void main(String[] args) {
+         TelaPrincipal tl = new TelaPrincipal();
+        tl.setTitle("Tela Principal");
+        tl.setLocationRelativeTo(null);
+        tl.pack();
+        tl.setVisible(true);
+    }
+    
+}
